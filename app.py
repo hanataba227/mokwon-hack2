cd1388413d8e7b2b49831fc27549260d9e6b9b45
hanataba227 <gabriel227@naver.com>
2025-09-28 06:08:19 +0900

    config 수정

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.set_page_config(page_title="Ko-Connect", layout="wide")
+st.set_page_config(page_title="Konnect", layout="wide")
 
 import os
 from dotenv import load_dotenv
